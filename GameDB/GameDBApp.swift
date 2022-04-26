2dfac54f18f34f5dd6249c4acd079fba55c9c943
Parshva <36695274+shahparshva72@users.noreply.github.com>
2022-04-26 02:35:06 -0500

    Initial Commit

---
+++ GameDB/GameDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDBApp.swift
+//  GameDB
+//
+//  Created by Parshva Shah on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct GameDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
